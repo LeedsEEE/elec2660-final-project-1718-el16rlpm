ebce1482f977581a03056c145531c259954d2463
default <el16rlpm@eentpc248.leeds.ac.uk>
2017-11-21 15:31:37 +0000

    Initial Commit

---
+++ The Jogging App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The Jogging App
+//
+//  Created by Ricardo Pereira Marques da Silva [el16rlpm] on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ The Jogging App/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  The Jogging App
+//
+//  Created by Ricardo Pereira Marques da Silva [el16rlpm] on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
